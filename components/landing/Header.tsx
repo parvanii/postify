e8bdec75374fde829f7344bf9aec0e0518bcddd8
Parvani Sood <sood.parvani24@gmail.com>
2025-11-19 19:41:06 +0530

    updated hero section

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -73,8 +73,14 @@ export default function Header() {
          
           <SignInButton>
             <Button
-              variant="ghost"
-              className="text-white/90 text-[15px] rounded-[30px]  font-semibold"
+            variant="ghost"
+            
+              className="cursor-pointer transition-all 
+              bg-white text-black px-6 py-2 rounded-full
+              border-black
+              border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+              active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
+            
             >
               Sign in
             </Button>
@@ -83,7 +89,13 @@ export default function Header() {
        
           <SignUpButton>
             <Button
-              className="px-5 py-2 bg-white text-black rounded-[30px] text-[15px] font-semibold hover:bg-gray-100"
+              variant="ghost"
+              className="cursor-pointer transition-all 
+              bg-white text-black px-6 py-2 rounded-full
+              border-black
+              border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+              active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
+         
             >
               Sign up
             </Button>
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -15,45 +15,77 @@ const openSans = Open_Sans({
 export default function Hero() {
   return (
     <section className="w-full" style={{ backgroundColor: "#5523E8" }}>
-      <div className="max-w-7xl mx-auto px-6 lg:px-8 py-28 lg:py-36 relative overflow-hidden">
+      <div className="max-w-7xl mx-auto px-6 lg:px-8 py-28 lg:py-36 relative overflow-visible">
 
         <div className="text-center max-w-3xl mx-auto">
           <h1
             className={`${poppins.className} text-white text-4xl sm:text-5xl md:text-6xl font-bold leading-tight`}
           >
-            Create Content that{" "}
-            <span className="inline-block font-bold" style={{ color: "#ADD402" }}>
-              Clicks
-            </span>
+            Make{" "}
+            <span className="relative inline-block px-3 py-1 border-2 border-[#ADD402] rounded-xl font-semibold ">
+              Content
+
+              <span className="absolute -top-2 -right-2 text-[#ADD402] text-xl">✦</span>
+
+              <span className="absolute -bottom-2 -left-2 text-[#ADD402] text-xl">✦</span>
+            </span>{" "}
+
+            People Can't <span className="text-[#ADD402]">Ignore </span>
           </h1>
+
 
           <p className={`${openSans.className} text-white/80 mt-6 text-base sm:text-lg max-w-xl mx-auto`}>
             Because great posts shouldn&apos;t take hours.
           </p>
 
-          {/* Buttons */}
+
           <div className={`${openSans.className} mt-10 flex items-center justify-center gap-4`}>
-            
-            {/* SIGN UP BUTTON */}
+
+
             <SignUpButton>
-              <button className="px-7 py-3 rounded-[20px] bg-white text-black text-sm font-semibold shadow-sm hover:shadow-md transition-all">
+              <button
+                className="cursor-pointer transition-all 
+  bg-white text-black px-6 py-2 rounded-full
+  border-black
+  border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+  active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
+              >
                 Get started
               </button>
             </SignUpButton>
 
+
             {/* More Info */}
-            <Link href="/about"  rel="noreferrer">
+            <Link href="/about" target="" rel="noreferrer">
               <button
-                className="px-7 py-3 rounded-[20px] text-white text-sm font-semibold"
-                style={{ backgroundColor: "#E823B6" }}
+                className="cursor-pointer transition-all 
+  bg-[#E823B6] text-white px-6 py-2 rounded-full
+  border-[#11020d]
+  border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+  active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
               >
                 More info
               </button>
             </Link>
           </div>
         </div>
+        <div className="absolute top-32 left-0 transform -translate-x-1/4">
+          <img src="/facebook.png" alt="facebook card" className="w-70 drop-shadow-lg rounded-xl" />
+        </div>
 
-        {/* Background Blobs */}
+        <div className="absolute bottom-20 left-16">
+          <img src="/x.png" alt="x card" className="w-70 drop-shadow-lg rounded-xl" />
+        </div>
+
+        <div className="absolute top-40 right-0 transform translate-x-1/4">
+          <img src="/reddit.png" alt="reddit card" className="w-70 drop-shadow-lg rounded-xl" />
+        </div>
+
+        <div className="absolute bottom-10 right-20">
+          <img src="/instagram.png" alt="instagram card" className="w-70 drop-shadow-lg rounded-xl" />
+        </div>
+
+
         <div
           className="absolute -left-40 -top-32 w-[520px] h-[520px] rounded-full blur-3xl opacity-20"
           style={{
